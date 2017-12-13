f0a3139d28f7e00340552ff83e66c96f60e9a423
Rafael Lorandi <coolparadox@gmail.com>
2017-12-13 14:45:02 -0200

    Cpf model structure updated

--- api/models/models.go
+++ api/models/models.go
@@ -6,8 +6,12 @@ import (
 
 type (
 	Cpf struct {
-		Id       bson.ObjectId `bson:"_id,omitempty" json:"id"`
-		Name     string        `json:"name"`
-		LastName string        `json:"lastname"`
+		Id          bson.ObjectId `bson:"_id,omitempty" json:"id"`
+		Cpf         string        `json:"cpf"`
+		IsCnpj      bool          `json:"is_cnpj"`
+		Blacklisted bool          `json:"blacklisted"`
+		Name        string        `json:"name"`
+		Comment     string        `json:"comment"`
+		// TODO(Rafael): further fields? phone address etc.
 	}
 )
