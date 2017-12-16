748b66b85b3850256d7b01647c23182a70af9c9f
Rafael Lorandi <coolparadox@gmail.com>
2017-12-16 19:14:18 -0200

    todoapp label renamed to cpfapp

--- frontend/src/root.js
+++ frontend/src/root.js
@@ -13,4 +13,4 @@ import AppContainer from './containers/AppContainer';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<AppContainer />, document.getElementById('todoapp'));
+ReactDOM.render(<AppContainer />, document.getElementById('cpfapp'));
